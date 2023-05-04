7ac4c2636b32b78937d833d2d2859bc556fc96bd
Saima Akter <74361691+Saimatonni@users.noreply.github.com>
2023-05-04 20:28:38 +0600

    Add files via upload

---
+++ public/chat-app/src/pages/Chat.jsx
@@ -0,0 +1,98 @@
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { allUsersRoute , host} from "../utils/APIRoutes";
+import Contacts from "../components/Contacts";
+import Welcome from "../components/Welcome";
+import ChatContainer from "./ChatContainer";
+import { io } from "socket.io-client";
+
+
+function Chat() {
+  const navigate = useNavigate();
+  const socket = useRef();
+  const [contacts, setContacts] = useState([]);
+  const [currentChat, setCurrentChat] = useState(undefined);
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  useEffect(() => {
+    async function fetchData() {
+      if (!localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+        navigate("/login");
+      } else {
+        setCurrentUser(
+          JSON.parse(localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY))
+        );
+      }
+    }
+    fetchData();
+  }, []);
+
+
+
+  useEffect(() => {
+    if (currentUser) {
+      socket.current = io(host);
+      socket.current.emit("add-user", currentUser._id);
+    }
+  }, [currentUser]);
+  
+
+  useEffect(() => {
+    async function fetchData() {
+      if (currentUser) {
+        if (currentUser.isAvatarImageSet) {
+          const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+          setContacts(data.data);
+        } else {
+          navigate("/setAvatar");
+        }
+      }
+    }
+    fetchData();
+  }, [currentUser]);
+  
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  };
+
+  return (
+    <>
+      <Container>
+        <div className="container">
+        <Contacts contacts={contacts} changeChat={handleChatChange} />
+        {currentChat === undefined ? (
+            <Welcome />
+          ) : (
+            <ChatContainer currentChat={currentChat} socket={socket} />
+          )}
+        </div>
+      </Container>
+    </>
+  )
+}
+
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #000000;
+  .container {
+    height: 100vh;
+    width: 100vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
+
+export default Chat
