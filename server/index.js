aedeb8024bf48ce274e695f3dff5e8616f29ad69
Saima Akter <74361691+Saimatonni@users.noreply.github.com>
2023-05-15 20:43:49 +0600

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -44,6 +44,7 @@ io.on("connection", (socket) => {
 
   socket.on("send-msg", (data) => {
     const sendUserSocket = onlineUsers.get(data.to);
+    // console.log(data);
     if (sendUserSocket) {
       socket.to(sendUserSocket).emit("msg-recieve", data.msg);
     }
